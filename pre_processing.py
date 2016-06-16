2a05cd772fef1bc8d12e38e94a26c70afed7006c
akshitajha <zenith158@gmail.com>
2016-06-16 21:26:49 +0530

    Add xlrd pre-processing file

--- pre_processing.py
+++ pre_processing.py
@@ -1,6 +1,7 @@
 from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
 import sys
+import xlrd
 
 # 6th field is the post id in answers
 # 4th field is the post id in posts
@@ -8,64 +9,90 @@ import sys
 
 data = {}
 
-
-def func_v(pid_col, u_col, d_col, fp):
-
-	for each_line in fp:
-		if pid_col in data:
-			data[pid_col].append(u_col)
-			data[pid_col].append(d_col)
-
-def func(pid_col, text_col, fp):
+def func(text, post_id):
 
 	questions = []
 	stop = stopwords.words('english')
 
-	for each_line in fp:
-		x = (each_line.lower()).split(",")
-		post_id, text = x[pid_col], x[text_col]
+	
+	x = (text.lower()).split(' ')
 
-		#print post_id, text
+	soup = BeautifulSoup(text)
+	text = soup.getText()
 
-		if len(text.strip()) == 0:
-			return
+	for each_word in text.split():
+		s = ''
+		if each_word not in stop:
+			s = s + each_word + ' '
+
+	data[post_id].append(text) 
+
+def main():
+
+	# python pre_processing.py data/sample_questions_from_AXC.xls data/sample_answer_from_AXC.xls data/sample_posts_upvotes_downvotes.xls > out.csv
+	
+	# questions
+	xl_workbook = xlrd.open_workbook(sys.argv[1])
+	xl_sheet = xl_workbook.sheet_by_index(0)
+
+	for i in range(0, xl_sheet.nrows):
+
+		post_id = xl_sheet.cell(i,0).value
+		question = xl_sheet.cell(i,3).value
+
+		if len(question) == 0:
+			continue
+
+		#print post_id, question
 
 		if post_id not in data:
 			data[post_id] = []
 
-		soup = BeautifulSoup(text)
-		text = soup.getText()
+		func(question, post_id)
 
-		for each_word in text.split():
-			s = ''
-			if each_word not in stop:
-				s = s + each_word + ' '
+	# answers
+	xl_workbook = xlrd.open_workbook(sys.argv[2])
+	xl_sheet = xl_workbook.sheet_by_index(0)
+	#print xl_sheet
 
-		data[post_id].append(text) 
+	for i in range(0, xl_sheet.nrows):
+		post_id = xl_sheet.cell(i,5).value
+		answer = xl_sheet.cell(i,1).value
+		
+		if post_id not in data:
+			continue
+			#data[post_id] = []
 
-def main():
-	fp_q = open(sys.argv[1]).readlines()
-	fp_a = open(sys.argv[2]).readlines()
-	fp_v = open(sys.argv[3]).readlines()
+		func(answer, post_id)
 
-	# func(pid_col, text_col, fp)
-	func(0, 3, fp_q) # questions
-	func(5, 1, fp_a) # answers
 
-	func_v(2, 0, 1, fp_v)	# vote(pid, upvote, downvote, file)
+	# votes
+	xl_workbook = xlrd.open_workbook(sys.argv[3])
+	xl_sheet = xl_workbook.sheet_by_index(0)
+	#print xl_sheet
+
+	for i in range(0, xl_sheet.nrows):
+		post_id = xl_sheet.cell(i,3).value
+		uvote = xl_sheet.cell(i,0).value
+		dvote = xl_sheet.cell(i,1).value
+		
+		if post_id not in data:
+			continue
+			#data[post_id] = []
+
+		data[post_id].append(uvote)
+		data[post_id].append(dvote)
 
 	for key in data:
 		print key, ",",
 		
-		for v in data[key]:
-			try:
-				print v, ",",
-			except:
-				print v.encode('utf-8'), ",",
-		print
-		#for v in data[key]:
-			#print v, ",",
-		#print
+	 	for v in data[key]:
+	 		try:
+	 			print v, ",",
+	 		except:
+	 			print v.encode('utf-8'), ",",
+	 	print
+
 
 if __name__ == '__main__':
 	main()
