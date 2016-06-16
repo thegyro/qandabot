b6637b288fff7e52d27d14591d07657a5c5c0c5d
PoorvaRane <poorvarane99@gmail.com>
2016-06-16 23:26:41 +0530

    Basic UI for chatbot ready

--- webapp/echoApp/models.py
+++ webapp/echoApp/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# # Create your models here.
+# class Conversation(models.Model):
+# 	conversation_id = models.
+# 	message = models.models.TextField(blank=False,null=False, default='0')
+#     conversation_timestamp = models.DateTimeField('Sent at:', auto_now_add=True)
+
+# class Account(models.Model):
+#     user = models.OneToOneField(User)
+#     conversation_id = models.ForeignKey(Conversation,null=False)
