62bab3ef1854e36051a87f098c532d661034fac9
PoorvaRane <poorvarane99@gmail.com>
2016-06-16 16:33:33 +0530

    Basic view to accept POST request

--- webapp/echoApp/views.py
+++ webapp/echoApp/views.py
@@ -1,3 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+from django.shortcuts import render_to_response
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext,loader
+from django.contrib.auth import authenticate, login ,logout
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
+from django import forms
 
 # Create your views here.
+
+def home(request):
+	if request.method == 'POST':
+		return HttpResponse('Hello')
