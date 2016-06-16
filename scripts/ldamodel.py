5bf2236c05bb463b625553079f423b5760e9b421
bhargavvader <bhargavvader@gmail.com>
2016-06-16 17:11:59 +0530

    LDA model

---
+++ scripts/ldamodel.py
@@ -0,0 +1,26 @@
+"""
+
+This is a file which creates the 'domains' after the questions and answers have been pre-processed.
+It also has top answers in a domain.
+
+"""
+
+from gensim.models import ldamodel
+from gensim.corpora import Dictionary
+
+
+q_corpus = []
+a_corpus = []
+for line in open("csvfile.txt"):
+	q_corpus.append(line.split[1].split())
+	a_corpus.append(line.split[2].split())
+
+q_dictionary = Dictionary(q_corpus)
+q_corpus = [q_dictionary.doc2bow(text) for text in q_corpus]
+
+a_dictionary = Dictionary(a_corpus)
+a_corpus = [a_dictionary.doc2bow(text) for text in a_corpus]
+
+
+q_model = ldamodel.LdaModel(corpus, num_topics=5)
+a_model = ldamodel.LdaModel(corpus, num_topics=5)
