e20a476014696c0ab421baff05a18cc18989bab8
akshitajha <zenith158@gmail.com>
2016-06-17 11:08:02 +0530

    Add questions

--- scripts/doc2vec.py
+++ scripts/doc2vec.py
@@ -11,21 +11,26 @@ def read_corpus(fname, tokens_only=False):
 
  
 corpus = list(read_corpus("intuit_questions.txt"))
-model = gensim.models.doc2vec.Doc2Vec(size=50, min_count=2, alpha=0.025, min_alpha=0.025)
-#model =  gensim.models.doc2vec.Doc2Vec.load("Intuit_Model.doc2vec")
-model.build_vocab(corpus)
-#model.sort_vocab()
+# model = gensim.models.doc2vec.Doc2Vec(size=50, min_count=2, alpha=0.025, min_alpha=0.025)
+# #model =  gensim.models.doc2vec.Doc2Vec.load("Intuit_Model.doc2vec")
+# model.build_vocab(corpus)
+# #model.sort_vocab()
 
-for epoch in range(10):
-    model.train(corpus)
-    model.alpha -= 0.002  # decrease the learning rate
-    model.min_alpha = model.alpha  # fix the learning rate, no decay
+# for epoch in range(10):
+#     model.train(corpus)
+#     model.alpha -= 0.002  # decrease the learning rate
+#     model.min_alpha = model.alpha  # fix the learning rate, no decay
 
-inferred_vector = model.infer_vector("I have problem filing taxes".split())
+# inferred_vector = model.infer_vector("What if I make a mistake".split())
+# sims = model.docvecs.most_similar([inferred_vector], topn=2)#len(model.docvecs))
+# #print sims
+
+# model.save("Intuit_Model.doc2vec");
+
+model =  gensim.models.doc2vec.Doc2Vec.load("Intuit_Model.doc2vec")
+inferred_vector = model.infer_vector("I am unable to fill my tax form".split())
 sims = model.docvecs.most_similar([inferred_vector], topn=2)#len(model.docvecs))
 #print sims
-
-model.save("Intuit_Model.doc2vec");
 
 for label, index in [('MOST', 0), ('MOST',1)]:#('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
     #print(u'%s %s: %s\n' % (label, sims[index], ' '.join(corpus[sims[index][0]].words)))
